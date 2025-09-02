fe642a1bb34cb11aed64983619187abfdb1a04e0
nawawan <mizutake0428@yahoo.co.jp>
2025-09-02 14:32:24 +0000

    feat: dispkay rectangle on a canvas

--- frontend/wasm/src/maze/draw_rect.rs
+++ frontend/wasm/src/maze/draw_rect.rs
@@ -4,5 +4,9 @@ use web_sys::{CanvasRenderingContext2d};
 
 pub fn run(ctx: &CanvasRenderingContext2d) -> Result<(), JsValue> {
 
+    ctx.begin_path();
+    ctx.rect(10.0, 10.0, 20.0, 30.0);
+    ctx.stroke();
+
     Ok(())
 }
