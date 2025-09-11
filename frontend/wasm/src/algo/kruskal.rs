ef5803942a7028bb59e1f4e4b7312ae0f9e2f45c
nawawan <mizutake0428@yahoo.co.jp>
2025-09-11 14:04:25 +0000

    feat: implement parameterized test

--- frontend/wasm/src/algo/kruskal.rs
+++ frontend/wasm/src/algo/kruskal.rs
@@ -1,9 +1,8 @@
-use rand::prelude::*;
 use rand::SeedableRng;
+use rand::prelude::*;
 
-use crate::algo::unionfind::UnionFind;
 use crate::algo::grid::grid_edges;
-
+use crate::algo::unionfind::UnionFind;
 
 enum KruskalResultEdge {
     Used,
@@ -11,18 +10,36 @@ enum KruskalResultEdge {
 }
 
 // 縦heightマス・横widthマスのグリッドグラフについて、最小全域木を作成したときに使用しなかった辺を返す
-pub fn extract_unused_maze_edges_by_kruskal(width: usize, height: usize, step: usize) -> Vec<(usize, usize)> {
-    let edges = arrange_random_edges(width, height, step);
-    kruskal(width * height, edges, KruskalResultEdge::Used)
-}
-
-pub fn extract_used_maze_edges_by_kruskal(width: usize, height: usize, step: usize) -> Vec<(usize, usize)> {
+pub fn extract_unused_maze_edges_by_kruskal(
+    width: usize,
+    height: usize,
+    step: usize,
+) -> Vec<(usize, usize)> {
+    if width <= step && height <= step {
+        return Vec::new();
+    }
     let edges = arrange_random_edges(width, height, step);
     kruskal(width * height, edges, KruskalResultEdge::Unused)
 }
 
+pub fn extract_used_maze_edges_by_kruskal(
+    width: usize,
+    height: usize,
+    step: usize,
+) -> Vec<(usize, usize)> {
+    if width <= step && height <= step {
+        return Vec::new();
+    }
+    let edges = arrange_random_edges(width, height, step);
+    kruskal(width * height, edges, KruskalResultEdge::Used)
+}
+
 // kruskal法によって最小全域木を作成し、使用しなかった辺を返す
-fn kruskal(node_size: usize, edges: Vec<(usize, usize)>, edge_result: KruskalResultEdge) -> Vec<(usize, usize)>{
+fn kruskal(
+    node_size: usize,
+    edges: Vec<(usize, usize)>,
+    edge_result: KruskalResultEdge,
+) -> Vec<(usize, usize)> {
     let mut unionfind = UnionFind::new(node_size);
     let mut unused_edge: Vec<(usize, usize)> = Vec::new();
     let mut used_edge: Vec<(usize, usize)> = Vec::new();
@@ -31,19 +48,18 @@ fn kruskal(node_size: usize, edges: Vec<(usize, usize)>, edge_result: KruskalRes
         if !unionfind.same(node_x, node_y) {
             unionfind.merge(node_x, node_y);
             used_edge.push((node_x, node_y));
-        }
-        else {
+        } else {
             unused_edge.push((node_x, node_y));
         }
     }
     match edge_result {
         KruskalResultEdge::Used => used_edge,
-        KruskalResultEdge::Unused => unused_edge
+        KruskalResultEdge::Unused => unused_edge,
     }
 }
 
 // ランダムな順番のgridグラフの辺を返す
-fn arrange_random_edges(width: usize, height: usize, step: usize) -> Vec<(usize, usize)>  {
+fn arrange_random_edges(width: usize, height: usize, step: usize) -> Vec<(usize, usize)> {
     let mut edges = grid_edges(width, height, step);
     let mut random_bytes = [0u8; 8];
     getrandom::getrandom(&mut random_bytes).unwrap();
@@ -54,12 +70,13 @@ fn arrange_random_edges(width: usize, height: usize, step: usize) -> Vec<(usize,
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
+    use rstest::*;
+    use std::{collections::HashSet, hash::Hash};
 
     use super::*;
 
     #[test]
-    fn create_minimum_spanning_tree(){
+    fn create_minimum_spanning_tree() {
         let node_size: usize = 5;
         let mut edges: Vec<(usize, usize)> = Vec::new();
 
@@ -68,13 +85,15 @@ mod tests {
             for j in i..node_size {
                 edges.push((i, j));
             }
-        } 
+        }
         let unused_edges = kruskal(node_size, edges.clone(), KruskalResultEdge::Used);
 
         // fetch node in spanning tree
         let mut set: HashSet<(usize, usize)> = HashSet::new();
-        unused_edges.into_iter().for_each(|x| {set.insert(x);});
-        let mut contains_integer : HashSet<usize> = HashSet::new();
+        unused_edges.into_iter().for_each(|x| {
+            set.insert(x);
+        });
+        let mut contains_integer: HashSet<usize> = HashSet::new();
         for (x, y) in edges {
             if set.contains(&(x, y)) {
                 continue;
@@ -84,11 +103,10 @@ mod tests {
         }
 
         assert_eq!(node_size, contains_integer.len());
-
     }
 
     #[test]
-    fn return_just_right_unused_edges(){
+    fn return_just_right_unused_edges() {
         let node_size: usize = 5;
         let mut edges: Vec<(usize, usize)> = Vec::new();
 
@@ -96,7 +114,7 @@ mod tests {
             for j in i..node_size {
                 edges.push((i, j));
             }
-        } 
+        }
 
         let unused_edges = kruskal(node_size, edges.clone(), KruskalResultEdge::Unused);
 
@@ -104,33 +122,61 @@ mod tests {
         assert_eq!(edges.len(), unused_edges.len() + node_size - 1);
     }
 
-    #[test]
-    fn create_correct_minimum_spanning_tree_in_grid_graph(){
-        let width = 10;
-        let height = 20;
-        let unused_edges = extract_unused_maze_edges_by_kruskal(width, height, 1);
+    #[rstest]
+    #[case(10, 20, 1)]
+    #[case(10, 20, 2)]
+    #[case(1000, 1000, 13)]
+    #[case(5, 10, 7)]
+    fn create_correct_minimum_spanning_tree_in_grid_graph(
+        #[case] width: usize,
+        #[case] height: usize,
+        #[case] step: usize,
+    ) {
+        let nodes = create_spanning_tree(width, height, step);
+
+        let w = (width - 1) / step + 1;
+        let h = (height - 1) / step + 1;
+
+        assert_eq!(w * h, nodes.len());
+    }
+
+    #[rstest]
+    #[case(1, 1, 1)]
+    #[case(2, 10, 12)]
+    #[case(100, 100, 101)]
+    fn invalid_input_creates_empty_tree(
+        #[case] width: usize,
+        #[case] height: usize,
+        #[case] step: usize,
+    ) {
+        let nodes = create_spanning_tree(width, height, step);
+        assert_eq!(0, nodes.len());
+    }
+
+    fn create_spanning_tree(width: usize, height: usize, step: usize) -> HashSet<usize> {
+        let unused_edges = extract_unused_maze_edges_by_kruskal(width, height, step);
 
         // fetch node in spanning tree
         let mut set: HashSet<(usize, usize)> = HashSet::new();
-        unused_edges.into_iter().for_each(|x| {set.insert(x);});
+        unused_edges.into_iter().for_each(|x| {
+            set.insert(x);
+        });
 
         let mut nodes: HashSet<usize> = HashSet::new();
-        for i in 0..height {
-            for j in 0..width {
-                let vertical = (i * width + j, (i + 1) * width + j);
-                let horizontal = (i * width + j, i * width + j + 1);
-                if i + 1 < height && !set.contains(&vertical) {
+        for i in (0..height).step_by(step) {
+            for j in (0..width).step_by(step) {
+                let vertical = (i * width + j, (i + step) * width + j);
+                let horizontal = (i * width + j, i * width + j + step);
+                if i + step < height && !set.contains(&vertical) {
                     nodes.insert(i * width + j);
-                    nodes.insert((i + 1) * width + j);
+                    nodes.insert((i + step) * width + j);
                 }
-                if j + 1 < width && !set.contains(&horizontal){
+                if j + step < width && !set.contains(&horizontal) {
                     nodes.insert(i * width + j);
-                    nodes.insert(i * width + j + 1);
+                    nodes.insert(i * width + j + step);
                 }
             }
         }
-
-        assert_eq!(width * height, nodes.len());
+        nodes
     }
-
 }
