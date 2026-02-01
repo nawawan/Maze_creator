31c2d66094ecbb960f47263ccd213b75f190456c
nawawan <mizutake0428@yahoo.co.jp>
2026-02-01 18:32:45 +0900

    chore: add Send+Sync trait for blogRepository

--- backend/storage/src/lib.rs
+++ backend/storage/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod repository;
 pub mod blog_repository;
 
-pub mod model;
 pub mod database;
--- backend/usecase/src/repository/blog.rs
+++ backend/usecase/src/repository/blog.rs
@@ -2,6 +2,6 @@ use super::super::model::blog::*;
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait BlogRepository {
+pub trait BlogRepository: Send + Sync {
     async fn get_blogs(&self, filter: BlogFilter) -> Vec<Blog>;
 }
